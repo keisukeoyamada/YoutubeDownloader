04fdc93e1a7ca2ddfc34789b2b0fd98043026abe
keisuke.oyamada <oyamada0601@gmail.com>
2025-08-05 00:10:24 +0900

    fix:option and output path

--- YT_dL.py
+++ YT_dL.py
@@ -1,12 +1,19 @@
 # liks.jsonにDLしたいyoutubeリンクを記載しておく。それらをまとめてダウンロードするプログラム。
 import json
+
 from yt_dlp import YoutubeDL
 
-with open('download_list.json', 'r') as f:
+with open("download_list.json", "r") as f:
     data = json.load(f)
-    links = data['urls'] # JSON内でリンクが "urls" キーにあると仮定
-
-yt_opts = YoutubeDL({'format':'mp4'})
+    links = data["urls"]  # JSON内でリンクが "urls" キーにあると仮定
+ydl_opts = {
+    "format": "bestvideo+bestaudio/best",
+    "merge_output_format": "mp4",  # これを外せばmkv形式でDLされる
+    # "writeautomaticsub": True,
+    "outtmpl": "../../Downloads/YLdownloads/%(title)s.%(ext)s",
+    # "subtitleslangs": ["ja"],
+}
+yt_opts_instance = YoutubeDL(ydl_opts)
 
 for link in links:
-    yt_opts.download([link])
+    yt_opts_instance.download([link])
