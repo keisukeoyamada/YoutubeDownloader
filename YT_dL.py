df1d565b2115b12ac7ead0b24be77fd17377f9d9
keisuke.oyamada <oyamada0601@gmail.com>
2025-08-14 17:01:09 +0900

    fix:YT_dl.py subtitle

--- YT_dL.py
+++ YT_dL.py
@@ -8,10 +8,10 @@ with open("download_list.json", "r") as f:
     links = data["urls"]  # JSON内でリンクが "urls" キーにあると仮定
 ydl_opts = {
     "format": "bestvideo+bestaudio/best",
-    "merge_output_format": "mp4",  # これを外せばmkv形式でDLされる
-    # "writeautomaticsub": True,
-    "outtmpl": "../../Downloads/YLdownloads/%(title)s.%(ext)s",
-    # "subtitleslangs": ["ja"],
+    "merge_output_format": "mkv",  # これを外せばmkv形式でDLされる
+    "writeautomaticsub": True,
+    "outtmpl": "../../Downloads/YLdownloads/%(title)s/%(title)s.%(ext)s",
+    "subtitleslangs": ["en", "ja"],  # 必要に応じて言語を追加
 }
 yt_opts_instance = YoutubeDL(ydl_opts)
 
